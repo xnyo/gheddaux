a1ec372728f8db3d8618d218ef99908e81891b4f
Giuseppe Guerra <to@nyo.zz.mu>
2019-05-20 17:23:41 +0200

    Update to discord.py 1.1.1

--- nazibot.py
+++ nazibot.py
@@ -2,6 +2,7 @@ import asyncio
 import time
 
 from discord.ext import commands
+from discord import TextChannel
 from signal import signal, SIGINT
 
 from singletons.bot import Bot
@@ -18,8 +19,8 @@ async def connect_db():
     await Database().connect(Config()["DB_DSN"], loop=asyncio.get_event_loop())
     await Database().execute("""
 CREATE TABLE IF NOT EXISTS `banned_words` (
-    `id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-    `word`	TEXT UNIQUE
+    `id`        INTEGER PRIMARY KEY AUTOINCREMENT,
+    `word`      TEXT UNIQUE
 );""")
 
     # Load forbidden words
@@ -37,11 +38,11 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     # Don't do anything in private messages
-    if message.server is None:
+    if type(message.channel) is not TextChannel:
         return
 
     # Check server id
-    if not message.server.id.isdecimal() or int(message.server.id) not in Config()["SERVER_IDS"]:
+    if message.channel.guild.id not in Config()["SERVER_IDS"]:
         return
 
     # Process commands
@@ -56,9 +57,9 @@ async def on_message(message):
         message_words = message.content.lower()
         for banned_word in banned_words:
             if banned_word in message_words:
-                await bot.delete_message(message)
+                await message.delete()
                 print(
-                    "=> Deleted message from {} [{}] ({})".format(message.author.name, message.author.id, message.content)
+                    f"=> Deleted message from {message.author.name} [{message.author.id}] ({message.content})"
                 )
 
 
@@ -67,13 +68,13 @@ async def on_message(message):
 async def censor(ctx, word=None):
     # Syntax check
     if word is None:
-        await bot.say("**Invalid syntax**. `!censor word`")
+        await ctx.message.channel.send("**Invalid syntax**. `!censor word`")
         return
 
     # Make sure the word is not already censored
     word = word.lower()
     if word in WordsCache():
-        await bot.say("**{}** is already censored!".format(word))
+        await ctx.message.channel.send("**{}** is already censored!".format(word))
         return
 
     # Censor new word and reload cache
@@ -81,7 +82,7 @@ async def censor(ctx, word=None):
     await WordsCache().reload()
 
     # Bot's reply
-    await bot.say("**{}** is now censored!".format(word))
+    await ctx.message.channel.send("**{}** is now censored!".format(word))
 
 
 @bot.command(pass_context=True, no_pm=True)
@@ -89,13 +90,13 @@ async def censor(ctx, word=None):
 async def uncensor(ctx, word=None):
     # Syntax check
     if word is None:
-        await bot.say("**Invalid syntax**. `!uncensor word`")
+        await ctx.message.channel.send("**Invalid syntax**. `!uncensor word`")
         return
 
     # Make sure the word is censored
     word = word.lower()
     if word not in WordsCache():
-        await bot.say("**{}** is not censored!".format(word))
+        await ctx.message.channel.send("**{}** is not censored!".format(word))
         return
 
     # Censor new word and reload cache
@@ -103,19 +104,19 @@ async def uncensor(ctx, word=None):
     await WordsCache().reload()
 
     # Bot's reply
-    await bot.say("**{}** isn't censored anymore!".format(word))
+    await ctx.message.channel.send("**{}** isn't censored anymore!".format(word))
 
 
 @bot.command(pass_context=True, no_pm=True)
 @checks.admin_only()
 async def censoredwords(ctx, word=None):
-    await bot.say("**Censored words:** {}".format(", ".join(WordsCache())))
+    await ctx.message.channel.send("**Censored words:** {}".format(", ".join(WordsCache())))
 
 try:
-    print("""\033[92m                 _ _       _   
-     ___ ___ ___|_| |_ ___| |_ 
+    print("""\033[92m                 _ _       _
+     ___ ___ ___|_| |_ ___| |_
     |   | .'|- _| | . | . |  _|
-    |_|_|__,|___|_|___|___|_|  
+    |_|_|__,|___|_|___|___|_|
       Nazi Bot - Made by Nyo\033[0m\n""")
     print("=> Logging in")
     bot.loop.run_until_complete(connect_db())

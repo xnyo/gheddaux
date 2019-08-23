27e0b5d11d5e7470eaa9aeda690b733c80657c18
Giuseppe Guerra <to@nyo.zz.mu>
2019-08-23 13:05:12 +0200

    isin command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from singletons.bot import Bot
 
 from utils import checks
 
@@ -7,7 +8,28 @@ from utils import checks
 class Moderation(commands.Cog):
 	@commands.command()
 	@checks.privileged()
+	async def isin(self, ctx, user_id: int) -> None:
+		"""
+		<user_id> ~ Checks if '<user_id>' is in an allowed Discord server.
+		"""
+
+		u = ctx.bot.get_user(user_id)
+		if u is None:
+			await ctx.send("User not found")
+			return
+		profile = await u.profile()
+		if not any(x in Bot().allowed_server_ids for x in await u.profile().mutual_guilds):
+			await ctx.send("No. The specified user is not in any of the allowed guilds.")
+			return
+		await ctx.send(f"Yes. Username: `{u.username}`")
+
+	@commands.command()
+	@checks.privileged()
 	async def prune(self, ctx, how_many: int) -> None:
+		"""
+		<x> ~ Deletes '<x>' messages in the current channel.
+		"""
+
 		if how_many > 50:
 			await ctx.send("You can delete up to 50 messages")
 			return
@@ -17,6 +39,10 @@ class Moderation(commands.Cog):
 	@commands.command()
 	@checks.privileged()
 	async def pruneu(self, ctx, member: discord.User, how_many: int) -> None:
+		"""
+		<@user> <x> ~ Deletes '<x>' messages sent by '<@user>' in the current channel.
+		"""
+
 		if how_many > 50:
 			await ctx.send("You can delete up to 50 messages")
 			return
